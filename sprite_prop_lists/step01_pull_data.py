daa3110d0cd6fd7269a01204c06fdbf3ad557661
Alexander H. Prociuk <a.h.prociuk@gmail.com>
2026-06-16 22:59:53 -0400

    Added correct test to check for existence of files.

--- sprite_prop_lists/step01_pull_data.py
+++ sprite_prop_lists/step01_pull_data.py
@@ -1,3 +1,9 @@
-def file_exists(files_exist):
-    return files_exist
+import os
+
+def file_exists(mypath, myfiles):
+    all_files_exist = True
+    for myfile in myfiles:
+        file_exists = os.path.isfile(os.path.join(mypath, myfile))
+        all_files_exist = all_files_exist and file_exists
+    return all_files_exist
 
