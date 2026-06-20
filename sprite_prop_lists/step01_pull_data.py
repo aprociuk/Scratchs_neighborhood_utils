586cfdd6d2807bc1776ba73a6d81eae925c92843
Alexander H. Prociuk <a.h.prociuk@gmail.com>
2026-06-19 21:29:34 -0400

    Added log status print string to file exists function.

--- sprite_prop_lists/step01_pull_data.py
+++ sprite_prop_lists/step01_pull_data.py
@@ -2,8 +2,16 @@ import os
 
 def file_exists(mypath, myfiles):
     all_files_exist = True
+    status_str = f"\n\nChecking location: {mypath}"
+    
     for myfile in myfiles:
         file_exists = os.path.isfile(os.path.join(mypath, myfile))
         all_files_exist = all_files_exist and file_exists
-    return all_files_exist
+        
+        if file_exists:
+            status_str += f"\n -- Found: {myfile}"
+        else:
+            status_str += f"\n -- Missing: {myfile}"
+        
+    return all_files_exist, status_str
 
