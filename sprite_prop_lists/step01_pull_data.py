78882b44971f85dce360b94b0cb2f76386f40da0
Alexander H. Prociuk <a.h.prociuk@gmail.com>
2026-06-19 22:58:00 -0400

    Coded capture to disk of prior selected file to remember prior selected folder on browser refresh.

--- sprite_prop_lists/step01_pull_data.py
+++ sprite_prop_lists/step01_pull_data.py
@@ -1,4 +1,5 @@
 import os
+from _000_system_include.global_functions import global_parameters
 
 def file_exists(mypath, myfiles):
     all_files_exist = True
@@ -12,6 +13,29 @@ def file_exists(mypath, myfiles):
             status_str += f"\n -- Found: {myfile}"
         else:
             status_str += f"\n -- Missing: {myfile}"
-        
+            
     return all_files_exist, status_str
+
+
+def load_save_path(path=None):
+    # Create buffer to store prior chosen folder
+    # 1. Create file name
+    params=global_parameters(report_paths=False)
+    file_name = os.path.join(params['data_path'],".selected_file.txt")
+    
+    if path == None and os.path.isfile(file_name):
+        # 2. Retrieve the string from the file
+        with open(file_name, "r", encoding="utf-8") as file:
+            path = file.read()
+        return path
+    elif path == None and not os.path.isfile(file_name):
+        path = os.getcwd()
+        # 2. Save the string to a file
+        with open(file_name, "w", encoding="utf-8") as file:
+            file.write(path)
+        return path
+    else:
+        # 3. Save the string to a file
+        with open(file_name, "w", encoding="utf-8") as file:
+            file.write(path)
 
