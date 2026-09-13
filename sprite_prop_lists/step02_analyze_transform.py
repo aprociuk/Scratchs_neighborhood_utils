a4eff80489d54026a6b187b488ffca46b7b7fedc
Alexander H. Prociuk <a.h.prociuk@gmail.com>
2026-09-12 22:48:49 -0400

    Added template for creating sprite_prop_roadmap.

--- sprite_prop_lists/step02_analyze_transform.py
+++ sprite_prop_lists/step02_analyze_transform.py
@@ -4,4 +4,21 @@ import sqlite3
 
 
 def sprite_prop_roadmap(gparams):
-    print("Hello!")
+    
+    # Connect to the SQLite database
+    # Note: if the database did not exist, then this
+    #       command would create it.
+    conn = sqlite3.connect(os.path.join(params['sqlite3_dbs'],'scratchs_neighborhood.db'))
+    cursor = conn.cursor()
+    
+    cursor.executescript('''
+        drop table if exists sprite_prop_roadmap;
+        create table sprite_prop_roadmap as
+        select 
+        from ;
+    ''')
+    
+    superbowl_win_counts = pd.read_sql_query(
+            "select * from sprite_prop_roadmap", conn
+    )
+    
